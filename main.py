1c12b58950499933c320729bbb71f07fbfec251a
charliec999 <72533638+charliec999@users.noreply.github.com>
2024-10-01 17:31:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ window = pyglet.window.Window(width=800, height=400, caption='Chrome Dino Game M
 batch = pyglet.graphics.Batch()
 
 fullscreen = False
-dino_color = (255, 255, 255)
-cactus_color = (200, 50, 50)
-ground_color = (150, 150, 150)
+dino_color = (255, 0, 0)
+cactus_color = (0, 255, 0)
+ground_color = (255, 255, 0)
 
 class Dino:
     def __init__(self):
-        self.width = 40
-        self.height = 60
         self.x = 50
         self.y = 60
         self.y_velocity = 0
@@ -35,8 +33,6 @@ class Dino:
 
 class Cactus:
     def __init__(self, x):
-        self.width = 20
-        self.height = random.randint(30, 60)
         self.x = x
         self.y = 60
 
@@ -45,7 +41,16 @@ class Cactus:
 
 class Ground:
     def __init__(self):
-        self.y = 0
+        self.x1 = 0
+        self.x2 = 400
+
+    def update(self, dt, speed):
+        self.x1 -= speed * dt
+        self.x2 -= speed * dt
+        if self.x1 < -400:
+            self.x1 += 800
+        if self.x2 < -400:
+            self.x2 += 800
 
 class NeuralNetwork:
     def __init__(self):
@@ -59,10 +64,7 @@ class NeuralNetwork:
         self.weights += np.random.randn(3) * 0.05
 
 def check_collision(dino, cactus):
-    return (dino.x < cactus.x + cactus.width and 
-            dino.x + dino.width > cactus.x and 
-            dino.y < cactus.y + cactus.height and 
-            dino.y + dino.height > cactus.y)
+    return (dino.x < cactus.x + 20 and dino.x + 20 > cactus.x and dino.y < cactus.y + 20 and dino.y + 20 > cactus.y)
 
 dino = Dino()
 ground = Ground()
@@ -73,11 +75,11 @@ generation = 1
 population_size = 100
 current_network = 0
 networks = [NeuralNetwork() for _ in range(population_size)]
-game_speed = 300
+game_speed = 350
 difficulty_increase_rate = 0.01
 
-score_label = pyglet.text.Label('Score: 0', x=10, y=370, font_size=18, color=(255, 255, 255, 255))
-generation_label = pyglet.text.Label(f'Gen: {generation}', x=700, y=370, font_size=18, color=(255, 255, 255, 255))
+score_label = pyglet.text.Label('0', x=10, y=370, font_size=18, color=(255, 255, 255, 255), batch=batch)
+generation_label = pyglet.text.Label(f'Gen: {generation}', x=700, y=370, font_size=18, color=(255, 255, 255, 255), batch=batch)
 
 def reset_game():
     global dino, cacti, score, game_over, game_speed
@@ -85,33 +87,7 @@ def reset_game():
     cacti = []
     score = 0
     game_over = False
-    game_speed = 300
-
-def draw_dino():
-    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [dino.x, dino.y, dino.x + dino.width, dino.y,
-                                                        dino.x + dino.width, dino.y + dino.height, dino.x, dino.y + dino.height]),
-                         ('c3B', dino_color * 4))
-
-def draw_cacti():
-    for cactus in cacti:
-        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [cactus.x, cactus.y, cactus.x + cactus.width, cactus.y,
-                                                            cactus.x + cactus.width, cactus.y + cactus.height, cactus.x, cactus.y + cactus.height]),
-                             ('c3B', cactus_color * 4))
-
-def draw_ground():
-    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [0, ground.y, 800, ground.y,
-                                                        800, ground.y + 60, 0, ground.y + 60]),
-                         ('c3B', ground_color * 4))
-
-def draw_neural_network():
-    weights = networks[current_network].weights
-    for i, weight in enumerate(weights):
-        # Normalize weight to be in range [0, 1] for color
-        weight_color = max(0, min(1, weight))
-        color = (int(255 * weight_color), int(255 * (1 - weight_color)), 0)
-        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [50 + i * 60, 370, 50 + i * 60 + 40, 370,
-                                                            50 + i * 60 + 40, 370 + 20, 50 + i * 60, 370 + 20]),
-                             ('c3B', color * 4))
+    game_speed = 350
 
 def update(dt):
     global score, game_over, current_network, generation, game_speed, difficulty_increase_rate
@@ -135,6 +111,7 @@ def update(dt):
         return
 
     dino.update(dt)
+    ground.update(dt, game_speed)
 
     if random.randint(1, 100) == 1 and len(cacti) < 2:
         cacti.append(Cactus(800))
@@ -144,7 +121,7 @@ def update(dt):
         if cactus.x < -30:
             cacti.remove(cactus)
             score += 1
-            score_label.text = f'Score: {score}'
+            score_label.text = f'{score}'
             game_speed += 3
 
         if check_collision(dino, cactus):
@@ -155,7 +132,7 @@ def update(dt):
     if cacti:
         inputs = [
             (cacti[0].x - dino.x) / 800,
-            (cacti[0].height - dino.height) / 60,
+            (cacti[0].y - dino.y) / 60,
             dino.y_velocity / 15
         ]
         if networks[current_network].predict(inputs):
@@ -163,15 +140,26 @@ def update(dt):
 
     game_speed += difficulty_increase_rate
 
+def draw_neural_network():
+    for i in range(3):
+        x = (window.width - 120) / 2 + i * 60
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [x, 370, x + 40, 370, x + 40, 400, x, 400]),
+                             ('c3B', (255, 255, 255) * 4))
+        pyglet.text.Label(f'W{i}: {networks[current_network].weights[i]:.2f}', x=x + 5, y=375,
+                          font_size=12, color=(0, 0, 0, 255), batch=batch).draw()
+
 @window.event
 def on_draw():
     window.clear()
-    draw_ground()
-    draw_dino()
-    draw_cacti()
+    pyglet.graphics.draw(2, pyglet.gl.GL_QUADS, ('v2f', [0, 0, window.width, 0, window.width, 200, 0, 200]), 
+                         ('c3B', (100, 100, 100) * 4))
     draw_neural_network()
-    score_label.draw()
-    generation_label.draw()
+    if not game_over:
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [dino.x, dino.y, dino.x + 20, dino.y, dino.x + 20, dino.y + 20, dino.x, dino.y + 20]), 
+                             ('c3B', dino_color * 4))
+        for cactus in cacti:
+            pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [cactus.x, cactus.y, cactus.x + 20, cactus.y, cactus.x + 20, cactus.y + 20, cactus.x, cactus.y + 20]),
+                                 ('c3B', cactus_color * 4))
 
 @window.event
 def on_key_press(symbol, modifiers):
