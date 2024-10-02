87b43cb8967b358df7cbd2f76d7f0136705e47a7
charliec999 <72533638+charliec999@users.noreply.github.com>
2024-10-01 17:30:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ window = pyglet.window.Window(width=800, height=400, caption='Chrome Dino Game M
 batch = pyglet.graphics.Batch()
 
 fullscreen = False
-dino_image = pyglet.image.load('assets/dino.png')
-cactus_image = pyglet.image.load('assets/cactus.png')
-ground_image = pyglet.image.load('assets/ground.png')
+dino_color = (255, 255, 255)
+cactus_color = (200, 50, 50)
+ground_color = (150, 150, 150)
 
 class Dino:
     def __init__(self):
-        self.sprite = pyglet.sprite.Sprite(dino_image, x=50, y=60, batch=batch)
+        self.width = 40
+        self.height = 60
+        self.x = 50
+        self.y = 60
         self.y_velocity = 0
         self.is_jumping = False
 
@@ -22,33 +25,27 @@ class Dino:
             self.is_jumping = True
 
     def update(self, dt):
-        self.sprite.y += self.y_velocity
-        if self.sprite.y > 60:
+        self.y += self.y_velocity
+        if self.y > 60:
             self.y_velocity -= 50 * dt
         else:
-            self.sprite.y = 60
+            self.y = 60
             self.y_velocity = 0
             self.is_jumping = False
 
 class Cactus:
     def __init__(self, x):
-        self.sprite = pyglet.sprite.Sprite(cactus_image, x=x, y=60, batch=batch)
+        self.width = 20
+        self.height = random.randint(30, 60)
+        self.x = x
+        self.y = 60
 
     def update(self, dt, speed):
-        self.sprite.x -= speed * dt
+        self.x -= speed * dt
 
 class Ground:
     def __init__(self):
-        self.sprites = [
-            pyglet.sprite.Sprite(ground_image, x=0, y=0, batch=batch),
-            pyglet.sprite.Sprite(ground_image, x=400, y=0, batch=batch)
-        ]
-
-    def update(self, dt, speed):
-        for sprite in self.sprites:
-            sprite.x -= speed * dt
-            if sprite.x < -400:
-                sprite.x += 800
+        self.y = 0
 
 class NeuralNetwork:
     def __init__(self):
@@ -61,8 +58,11 @@ class NeuralNetwork:
     def mutate(self):
         self.weights += np.random.randn(3) * 0.05
 
-def check_collision(a, b):
-    return (a.x < b.x + b.width and a.x + a.width > b.x and a.y < b.y + b.height and a.y + a.height > b.y)
+def check_collision(dino, cactus):
+    return (dino.x < cactus.x + cactus.width and 
+            dino.x + dino.width > cactus.x and 
+            dino.y < cactus.y + cactus.height and 
+            dino.y + dino.height > cactus.y)
 
 dino = Dino()
 ground = Ground()
@@ -73,12 +73,11 @@ generation = 1
 population_size = 100
 current_network = 0
 networks = [NeuralNetwork() for _ in range(population_size)]
-game_speed = 350
+game_speed = 300
 difficulty_increase_rate = 0.01
 
-score_label = pyglet.text.Label('0', x=10, y=370, font_size=18, color=(255, 255, 255, 255), batch=batch)
-generation_label = pyglet.text.Label(f'Gen: {generation}', x=700, y=370, font_size=18, color=(255, 255, 255, 255), batch=batch)
-nn_info_label = pyglet.text.Label('', x=10, y=350, font_size=16, color=(255, 255, 0, 255), batch=batch)
+score_label = pyglet.text.Label('Score: 0', x=10, y=370, font_size=18, color=(255, 255, 255, 255))
+generation_label = pyglet.text.Label(f'Gen: {generation}', x=700, y=370, font_size=18, color=(255, 255, 255, 255))
 
 def reset_game():
     global dino, cacti, score, game_over, game_speed
@@ -86,7 +85,33 @@ def reset_game():
     cacti = []
     score = 0
     game_over = False
-    game_speed = 350
+    game_speed = 300
+
+def draw_dino():
+    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [dino.x, dino.y, dino.x + dino.width, dino.y,
+                                                        dino.x + dino.width, dino.y + dino.height, dino.x, dino.y + dino.height]),
+                         ('c3B', dino_color * 4))
+
+def draw_cacti():
+    for cactus in cacti:
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [cactus.x, cactus.y, cactus.x + cactus.width, cactus.y,
+                                                            cactus.x + cactus.width, cactus.y + cactus.height, cactus.x, cactus.y + cactus.height]),
+                             ('c3B', cactus_color * 4))
+
+def draw_ground():
+    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [0, ground.y, 800, ground.y,
+                                                        800, ground.y + 60, 0, ground.y + 60]),
+                         ('c3B', ground_color * 4))
+
+def draw_neural_network():
+    weights = networks[current_network].weights
+    for i, weight in enumerate(weights):
+        # Normalize weight to be in range [0, 1] for color
+        weight_color = max(0, min(1, weight))
+        color = (int(255 * weight_color), int(255 * (1 - weight_color)), 0)
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [50 + i * 60, 370, 50 + i * 60 + 40, 370,
+                                                            50 + i * 60 + 40, 370 + 20, 50 + i * 60, 370 + 20]),
+                             ('c3B', color * 4))
 
 def update(dt):
     global score, game_over, current_network, generation, game_speed, difficulty_increase_rate
@@ -110,31 +135,29 @@ def update(dt):
         return
 
     dino.update(dt)
-    ground.update(dt, game_speed)
 
     if random.randint(1, 100) == 1 and len(cacti) < 2:
         cacti.append(Cactus(800))
 
     for cactus in cacti:
         cactus.update(dt, game_speed)
-        if cactus.sprite.x < -30:
+        if cactus.x < -30:
             cacti.remove(cactus)
             score += 1
-            score_label.text = f'{score}'
+            score_label.text = f'Score: {score}'
             game_speed += 3
 
-        if check_collision(dino.sprite, cactus.sprite):
+        if check_collision(dino, cactus):
             game_over = True
             networks[current_network].fitness = score
             return
 
     if cacti:
         inputs = [
-            (cacti[0].sprite.x - dino.sprite.x) / 800,
-            (cacti[0].sprite.height - dino.sprite.height) / 60,
+            (cacti[0].x - dino.x) / 800,
+            (cacti[0].height - dino.height) / 60,
             dino.y_velocity / 15
         ]
-        nn_info_label.text = f'Inputs: {inputs}, Prediction: {networks[current_network].predict(inputs)}'
         if networks[current_network].predict(inputs):
             dino.jump()
 
@@ -143,17 +166,19 @@ def update(dt):
 @window.event
 def on_draw():
     window.clear()
-    batch.draw()
+    draw_ground()
+    draw_dino()
+    draw_cacti()
+    draw_neural_network()
+    score_label.draw()
+    generation_label.draw()
 
 @window.event
 def on_key_press(symbol, modifiers):
     global fullscreen
     if symbol == pyglet.window.key.F:
         fullscreen = not fullscreen
-        if fullscreen:
-            window.set_fullscreen(True)
-        else:
-            window.set_fullscreen(False)
+        window.set_fullscreen(fullscreen)
 
 pyglet.clock.schedule_interval(update, 1/120)
 pyglet.app.run()
